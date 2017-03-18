9db287385f5b35f99b022aba72610b0aa5d26c54
Valentine Kiselev <mrexox@yahoo.com>
2017-03-18 20:56:02 +0300

    new themes on forum added

--- javascript/forum.js
+++ javascript/forum.js
@@ -1,11 +1,29 @@
-	$(document).ready(function() {
+$(document).ready(function() {
 		var show_questions = $("a.show-questions.1");
 		show_questions.click(function(){
-			$("tr.questions.1").toggle(800);
+			$("tr.questions.1" ).toggle(200);
 			if (show_questions.html() == "+") {
 				show_questions.html("-")
 			} else {
 				show_questions.html("+")
 			}
 		});
-	})		
+		show_questions = $("a.show-questions.2");
+		show_questions.click(function(){
+			$("tr.questions.2" ).toggle(200);
+			if (show_questions.html() == "+") {
+				show_questions.html("-")
+			} else {
+				show_questions.html("+")
+			}
+		});
+		show_questions = $("a.show-questions.3");
+		show_questions.click(function(){
+			$("tr.questions.3" ).toggle(200);
+			if (show_questions.html() == "+") {
+				show_questions.html("-")
+			} else {
+				show_questions.html("+")
+			}
+		});
+})		
