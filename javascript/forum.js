41ca326db7aa8defc75dae040fbe01e941a72ea4
Valentine Kiselev <mrexox@yahoo.com>
2017-03-28 01:04:06 +0300

    ajax-like behaviour

--- javascript/forum.js
+++ javascript/forum.js
@@ -1,29 +1,49 @@
+var funcs = [];
 $(document).ready(function() {
-		var show_questions1 = $("a.show-questions.1");
-		show_questions1.click(function(){
-			$("tr.questions.1" ).toggle(200);
-			if (show_questions1.html() == "+") {
-				show_questions1.html("-")
-			} else {
-				show_questions1.html("+")
-			}
+
+
+	$.getJSON('data/forum.json', function(data) {
+		var table = document.getElementById('desk');
+		var th = table.getElementsByTagName('th')[0];
+		console.log(data);
+		var output;
+		$.each(data, function(id, item) {
+			var tr = document.createElement('tr');
+			tr.innerHTML += '<td>'  + '<a href="#" class="show-questions ' + (id+1) + '" onclick="funcs[' + id + ']()">' +'+</a> ' + item.theme + '</td>';
+			tr.innerHTML += '<td>' + item.description + '</td>';
+			tr.innerHTML += '<td align="center">' + item.author + '</td>';
+			tr.innerHTML += '<td align="center">' + item.questions.length + '</td>';
+			th.parentElement.parentElement.appendChild(tr);
+			var hiddenTr = document.createElement('tr');
+			hiddenTr.className = "questions "+ (+id+1);
+			hiddenTr.setAttribute("hidden", true);
+			// hidden
+			var insideTd = document.createElement('td');
+			insideTd.className = "questions";
+			insideTd.colSpan = "4";
+			var tbl = document.createElement('table');
+			tbl.className = "questions";
+			tbl.innerHTML = '<tr><th>Questions</th><th>Author</th><th>Answers</th></tr>';
+			$.each(item.questions, function(id, question) {
+				var trq = document.createElement('tr');
+				trq.innerHTML += '<td>' + question.title + '</td>';
+				trq.innerHTML += '<td align="center">' + question.author + '</td>';
+				trq.innerHTML += '<td align="center">' + question.answers + '</td>';
+				tbl.appendChild(trq);
+			});
+			insideTd.appendChild(tbl);
+			hiddenTr.appendChild(insideTd)
+				th.parentElement.parentElement.appendChild(hiddenTr);
+			funcs[id] = function(){
+				var style = "tr.questions." + (id+1);
+				$(style).toggle(200);
+				var a = document.getElementsByClassName('show-questions ' + (id+1))[0];
+				if (a.innerHTML == "+") {
+					a.innerHTML = "-"
+				} else {
+					a.innerHTML = "+"
+				}
+			}	
 		});
-		show_questions2 = $("a.show-questions.2");
-		show_questions2.click(function(){
-			$("tr.questions.2" ).toggle(200);
-			if (show_questions2.html() == "+") {
-				show_questions2.html("-")
-			} else {
-				show_questions2.html("+")
-			}
-		});
-		show_questions3 = $("a.show-questions.3");
-		show_questions3.click(function(){
-			$("tr.questions.3" ).toggle(200);
-			if (show_questions3.html() == "+") {
-				show_questions3.html("-")
-			} else {
-				show_questions3.html("+")
-			}
-		});
+	});
 })		
