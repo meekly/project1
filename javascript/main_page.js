b2b1a9a29c733356c46af7f70e296f1da63ef046
Admin <Admin>
2017-04-08 16:25:56 +0300

    main site 08.04.2017

---
+++ javascript/main_page.js
@@ -0,0 +1,17 @@
+//down.onmouseenter = a_hover(0.5);
+
+
+down.onmouseenter = function a_hover() {
+	m_form.style.opacity=0.5;
+	m_body.style.backgroundColor="#ffddbb";
+	down.style.opacity=1;
+}
+
+down.onmouseleave = function a_hover() {
+	m_form.style.opacity=1;
+	m_body.style.backgroundColor="#ffcc99";
+}
+
+//news.style.offsetTop = 2000 ;
+//document.documentElement.clientHeight
+//m_body.onscroll = function () {}
